116bf319ff078f72c1f6598928a69fb116a9d506
Benjamin <Benjamin@Benjamin-PC>
2013-10-15 16:20:35 +0200

    Un panier est maintenant validé seulement si tous ses articles sont OK

--- TP-ASSOCIATION/src/main/java/emn/association/services/impl/CommandeService.java
+++ TP-ASSOCIATION/src/main/java/emn/association/services/impl/CommandeService.java
@@ -46,16 +46,23 @@ public class CommandeService implements ICommandeService {
 
 	public boolean effectuerAchat(List<ArticlePanier> panier) {
 		ArticlePersistence serviceArticle = PersistenceServiceProvider.getService(ArticlePersistence.class);
+		
+		// Premiere loop pour checker si tout le panier est OK
 		for (ArticlePanier articlePanier : panier) {
 			Article articleAchete = serviceArticle.load(articlePanier.getArticle().getCode());
+			// Cas multi-utilisateur : L'article doit avoir les stocks suffisants pour valider la commande
 			if ((articleAchete.getStock()-articlePanier.getQuantite()) < 0) {
-				// TODO mettre a jour les articles du panier seulement si TOUS les articles dedans sont OK (le stock ne sera pas < 0)
-				// Pour l'instant ca met à jour les articles précédents la detection d'un article KO
 				return false; 
 			}
+		}
+		
+		// Ici le panier est OK
+		for (ArticlePanier articlePanier : panier) {
+			Article articleAchete = serviceArticle.load(articlePanier.getArticle().getCode());
 			articleAchete.setStock(articleAchete.getStock()-articlePanier.getQuantite());
 			serviceArticle.save(articleAchete);
 		}
+		
 		return suppressionPanier(panier);
 	}
 	
